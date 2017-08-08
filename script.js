c64f77534e4b6a111a3331bd4f6cb60fe4b71265
chrislacey89 <chrislacey89@gmail.com>
2017-08-08 06:44:20 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.body.style.backgroundColor = "red";
+
+document.getElementById("myDiv").style.backgroundColor = "blue";
+document.getElementById("myP").style.fontFamily = "sans-serif";
+document.getElementById("listitem").style.backgroundColor = "white";
+document.getElementById("listitem").style.backgroundColor = "white";
+
+
+nickname.innerHTML = "Chris";
+favorites.innerHTML = "Soccer & Bananas";
+hometown.innerHTML = "Orlando";
